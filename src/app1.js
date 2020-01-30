fc75a21be8c4cf34adc70e50e8c1e5b158e6b91d
KCVO1995 <943514589@qq.com>
2020-01-30 22:56:54 +0800

    重构，引入vue

--- src/app1.js
+++ src/app1.js
@@ -1,64 +1,48 @@
 import './app1.css'
-import $ from 'jquery'
-import Model from "./base/Model.js"
-import View from "./base/View"
+import Vue from 'vue'
 
 
 // m-数据
-const m = new Model({
-    data: {
-        n: parseFloat(localStorage.getItem('number'))
-    },
-    update: function (data) {
-        Object.assign(m.data, data)
-        this.trigger('m:update')
-        localStorage.setItem('number', data.n)
-    }
-})
 // v-视图
-
 // c-其他
 
 
 const init = (el) => {
-    new View ({
+    new Vue ({
         el: el,
-        data: m.data,
-        html: `
-        <div>
+        data: { n: parseFloat(localStorage.getItem('n')) },
+        methods: {
+            add() {
+                this.n += 1
+            },
+            minus() {
+                this.n -= 1
+            },
+            mul() {
+                this.n *= 2
+            },
+            div() {
+                this.n /= 2
+            }
+        },
+        watch: { // 监听
+            n(){
+                localStorage.setItem('n', this.n)
+            }
+        },
+        template: `
+        <section id="app1">
             <div class="output">
                 <span id="number">{{n}}</span>
             </div>
             <div class="action">
-                <button id="add1">+1</button>
-                <button id="minus1">-1</button>
-                <button id="mul2">*2</button>
-                <button id="divide2">÷2</button>
+                <button @click="add">+1</button>
+                <button @click="minus">-1</button>
+                <button @click="mul">*2</button>
+                <button @click="div">÷2</button>
             </div>
-        </div>
+        </section>
         ` ,
-        render(data) {
-            if (this.el.children().length !== 0) this.el.empty()
-            $(this.html.replace('{{n}}', data.n)).appendTo(this.el)
-        },
-        events:{
-            'click #add1': 'add',
-            'click #minus1': 'minus',
-            'click #mul2': 'mul',
-            'click #divide2': 'divide',
-        },
-        add(){
-            m.update({n: m.data.n + 1})
-        },
-        minus(){
-            m.update({n: m.data.n - 1})
-        },
-        mul(){
-            m.update({n: m.data.n * 2})
-        },
-        divide(){
-            m.update({n: m.data.n / 2})
-        },
     })
 }
 // 第一次渲染html
--- src/app2.js
+++ src/app2.js
@@ -1,48 +1,24 @@
 import  './app2.css'
-import $ from 'jquery'
-import Model from "./base/Model"
-import View from "./base/View"
-const localKey =  'app2.index'
-
-const m = new Model({
-    data: {
-        index: parseInt(localStorage.getItem(localKey) || 0)
-    },
-    update(data) {
-        Object.assign(m.data, data)
-        this.trigger('m:update')
-        localStorage.setItem(localKey, data.index)
-    }
-})
+import Vue from "vue"
 
 const init = (el) => {
-    new View ({
+    new Vue ({
         el: el,
-        data: m.data,
-        html: (index) => {
-            return   `
-            <div>
+        data: {
+            index: 0
+        },
+        template: `
+            <section id="app2">
                 <ol class="tab-bar">
-                    <li class="${index === 0 ? 'selected' : ' '}" data-index = '0'>1</li>
-                    <li class="${index === 1 ? 'selected' : ' ' }" data-index = '1'>2</li>
+                    <li :class="index === 0 ? 'selected' : '' " @click="index = 0">1</li>
+                    <li :class="index === 1 ? 'selected' : '' " @click="index = 1">2</li>
                 </ol>
                 <ol class="tab-content">
-                    <li class="${index === 0 ? 'active' : ' '}">内容1</li>
-                    <li class="${index === 1 ? 'active' : ' '}">内容2</li>
+                    <li :class="index === 0 ? 'active' : '' ">内容1</li>
+                    <li :class="index === 1 ? 'active' : '' ">内容2</li>
                 </ol>
-            </div>
-            ` },
-        render(data) {
-            if (this.el.children().length !== 0) this.el.empty()
-            $(this.html(data.index)).appendTo(this.el)
-        },
-        events:{
-            'click .tab-bar li': 'tabBar'
-        },
-        tabBar(e) {
-            const index = parseInt(e.currentTarget.dataset.index)
-            m.update({index : index})
-        }
+            </section>
+            `,
     })
 }
 
